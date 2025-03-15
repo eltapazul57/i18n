dd343f6155534e1ff42bfbb9e098d33d1f8810a3
Manuel Gómez <m.gomez49@uniandes.edu.co>
2025-03-15 16:10:30 -0500

    reto internacionalizacion listooooo

--- src/components/job.js
+++ src/components/job.js
@@ -1,14 +1,44 @@
 import React from "react";
+import { FormattedDate, FormattedNumber, FormattedPlural, useIntl } from "react-intl";
 
 const Job = (props) => {
+  const intl = useIntl(); // Obtener el idioma actual
+
   return (
     <tr>
       <th scope="row">{props.offer.id}</th>
       <td>{props.offer.name}</td>
       <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
+      <td>
+        <FormattedNumber 
+          value={props.offer.salary} 
+          style="decimal" 
+          useGrouping={true}
+        />
+        &nbsp;
+        <FormattedPlural 
+          value={props.offer.salary} 
+          one={intl.formatMessage({ id: "million" })} 
+          other={intl.formatMessage({ id: "millions" })}
+        />
+      </td>
       <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <td>
+        <FormattedDate
+          value={new Date(props.offer.date)}
+          year='numeric'
+          month='long'
+          day='numeric'
+          weekday='long'
+        />
+      </td>
+      <td>
+        <FormattedNumber 
+          value={props.offer.views} 
+          style="decimal" 
+          useGrouping={true}
+        />   
+      </td>
     </tr>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { IntlProvider } from "react-intl";
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
+
+// Detectar idioma del navegador
+const language = navigator.language.startsWith("es") ? "es-ES" : "en-EN";
+const messages = language === "es-ES" ? localeEsMessages : localeEnMessages;
+
+ReactDOM.render(
+    <IntlProvider locale={language} messages={messages}>
+        <JobsList />
+    </IntlProvider>, 
+    document.getElementById("root")
+);
